93335030658999461e3457bccd984fc745ab7618
Jinnux <nguyentandat26032001@gmail.com>
2022-11-20 23:53:47 +0700

    git init

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PollModule } from './poll/poll.module';
+import { PostModule } from './post/post.module';
+
+@Module({
+  imports: [AuthModule, UserModule, PollModule, PostModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
